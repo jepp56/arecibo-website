14bff06e94e45f08fc60b0f24cb697e2af80688e
Jessie <jessieluo56@gmail.com>
2025-07-24 22:32:49 -0700

    updated hero slider

---
+++ js/main.js
@@ -0,0 +1,48 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+
+document.getElementById('learnMoreBtn').onclick = function () {
+  window.location.href = 'about.html'; 
+};
+
+document.getElementById('joinUsBtn').onclick = function () {
+  document.getElementById('join-us').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+};
+
+
+function resetAutoSlide() {
+    clearInterval(runAuto);
+    runAuto = setInterval(() => {
+        showSlider('next');
+    }, timeAutoNext);
+}
+
+nextDom.onclick = function(){
+    showSlider('next');
+    resetAutoSlide();
+}
+prevDom.onclick = function(){
+    showSlider('prev');
+    resetAutoSlide();
+}
+let timeAutoNext = 7000;
+let runAuto = setInterval(() => {
+        showSlider('next');
+    }, timeAutoNext);
+
+function showSlider(type){
+    let itemSlider = document.querySelectorAll('.carousel .list .item');
+
+    if(type === 'next'){
+        listItemDom.appendChild(itemSlider[0]);
+    }else{
+        let positionLastItem = itemSlider.length -1;
+        listItemDom.prepend(itemSlider[positionLastItem]);
+    }
+}
+
+
